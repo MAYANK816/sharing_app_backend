157adab77c47b6ad2c1577ea352c93a20d86594a
MAYANK816 <mayank0218.cse19@chitkara.edu.in>
2022-05-27 16:03:00 +0530

    backend

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, { cors: { origin: "*" } });
 const cors = require("cors")
+
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
 app.use(cors());
 app.get('/', (req, res) => res.status(200).send("Hello Programmers"));
 
